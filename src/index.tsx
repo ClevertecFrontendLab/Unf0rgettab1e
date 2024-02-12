a9e84cf6638754f6a982151f1166eec2f9e2446b
AnthonyDavydchyk <62406462+AnthonyDavydchyk@users.noreply.github.com>
2024-02-12 14:12:11 +0300

    fix: correct import antd styles

--- src/index.tsx
+++ src/index.tsx
@@ -3,12 +3,11 @@ import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { HashRouter, Route, Routes } from 'react-router-dom';
 import 'antd/dist/antd.css';
+import 'normalize.css';
+import './index.scss';
 
 import { store } from '@redux/configure-store';
 import { MainPage } from './pages';
-
-import 'normalize.css';
-import './index.scss';
 
 const domNode = document.getElementById('root') as HTMLDivElement;
 const root = createRoot(domNode);
