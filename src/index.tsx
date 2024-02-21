1038c16bdbd51c3f285fe38c3bc9ff2062b83328
AnthonyDavydchyk <62406462+AnthonyDavydchyk@users.noreply.github.com>
2024-02-22 02:54:11 +0300

    init: start sprint-2

--- src/index.tsx
+++ src/index.tsx
@@ -1,25 +1,16 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import { Provider } from 'react-redux';
-import { HashRouter, Route, Routes } from 'react-router-dom';
+
 import 'antd/dist/antd.css';
 import 'normalize.css';
 import './index.scss';
-
-import { store } from '@redux/configure-store';
-import { MainPage } from './pages';
+import { App } from './App';
 
 const domNode = document.getElementById('root') as HTMLDivElement;
 const root = createRoot(domNode);
 
 root.render(
     <React.StrictMode>
-        <Provider store={store}>
-            <HashRouter>
-                <Routes>
-                    <Route path='/' element={<MainPage />} />
-                </Routes>
-            </HashRouter>
-        </Provider>
+        <App />
     </React.StrictMode>,
 );
