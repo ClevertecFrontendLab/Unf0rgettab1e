edd02c0552db7cdaaa8c0a179b34a44771b3913a
AnthonyDavydchyk <62406462+AnthonyDavydchyk@users.noreply.github.com>
2024-02-23 00:29:38 +0300

    refactor: add module paths for ts and vite

--- vite.config.ts
+++ vite.config.ts
@@ -12,13 +12,19 @@ export default defineConfig({
     resolve: {
         alias: {
             '@public': path.resolve(__dirname, 'public'),
+            '@api': path.resolve(__dirname, 'src/api'),
+            '@actions': path.resolve(__dirname, 'src/actions'),
             '@components': path.resolve(__dirname, 'src/components'),
             '@constants': path.resolve(__dirname, 'src/constants'),
             '@hooks': path.resolve(__dirname, 'src/hooks'),
             '@pages': path.resolve(__dirname, 'src/pages'),
             '@redux': path.resolve(__dirname, 'src/redux'),
+            '@routes': path.resolve(__dirname, 'src/routes'),
             '@types': path.resolve(__dirname, 'src/types'),
+            '@theme': path.resolve(__dirname, 'src/theme'),
             '@utils': path.resolve(__dirname, 'src/utils'),
+            '@models': path.resolve(__dirname, 'src/models'),
+            '@services': path.resolve(__dirname, 'src/services'),
         },
     },
 });
