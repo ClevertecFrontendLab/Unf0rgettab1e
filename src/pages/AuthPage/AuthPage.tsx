b2177da402164c9a257f627cfc913d44a7b74a61
AnthonyDavydchyk <62406462+AnthonyDavydchyk@users.noreply.github.com>
2024-02-23 03:53:19 +0300

    feat: navigate to main from auth

--- src/pages/AuthPage/AuthPage.tsx
+++ src/pages/AuthPage/AuthPage.tsx
@@ -1,12 +1,20 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Layout } from 'antd';
-
+import { Paths } from '@utils/constants/Paths';
+import { push } from 'redux-first-history';
+import { useAppDispatch } from '@hooks';
 import './AuthPage.scss';
 
 type AuthPageProps = {
     children: React.ReactNode;
 };
 export const AuthPage: React.FC<AuthPageProps> = ({ children }) => {
+    const dispatch = useAppDispatch();
+
+    useEffect(() => {
+        localStorage.getItem('token') ? dispatch(push(Paths.MAIN)) : '';
+    }, [dispatch]);
+
     return (
         <Layout className='auth-layout'>
             <Layout.Content>{children}</Layout.Content>
